7d8dc8f33c5685d210fca3c21052ed83ce8b56bb
Avoidy <95578426+Avoidy@users.noreply.github.com>
2022-02-21 16:43:32 +0100

    Add project files.

---
+++ MultiUrls/Utils/Startup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiUrls.Utils
+{
+    public partial class Startup
+    {
+        string buildLocation = AppDomain.CurrentDomain.BaseDirectory;
+        string osTime = DateTime.Now.ToString("dd-MM-yyyy");
+
+        public static void Routine(string versionNumber)
+        {
+
+        }
+
+    }
+}
